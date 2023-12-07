12ab7e02728a262f93e5e83a3c2c24f2ec4f9273
SanjaySolankiUS <150522812+SanjaySolankiUS@users.noreply.github.com>
2023-12-07 10:32:02 +0530

    Update Program.cs
    Updated with working code

--- ConsumerAPICSharp/Program.cs
+++ ConsumerAPICSharp/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace ConsumerAPICSharp;
+namespace ConsumerAPICSharp;
 using Confluent.Kafka;
 using System;
 
@@ -10,30 +10,43 @@ class Program
         Console.WriteLine("Hello, World! Consumer");
 
         var config = new ConsumerConfig {
-            BootstrapServers = "localhost:9092",
-            GroupId = "kafka-dotnet-getting-started"
+            BootstrapServers = "localhost:9092"
+            ,GroupId = "grp1"
+            ,AutoOffsetReset = AutoOffsetReset.Earliest
             };
 
-        using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+        using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
         {
             CancellationTokenSource cts = new CancellationTokenSource();
             consumer.Subscribe("ClassAtt");
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
             try {
+                // var incr = 1;
+                // consumer.ConsumeFromLatest("ClassAtt");
+                // consumer.MessageReceived += record => 
+                // {
+                //      Console.Write(incr.ToString() + " - " + Encoding.UTF8.GetString(record.Value));
+                //      Console.WriteLine();
+                //      incr++;
+                // };
+
                 while (true) {
                     var cr = consumer.Consume(cts.Token);
-                    Console.Write(cr.Message.Key);
-                    Console.Write(" --- " + cr.Message.Value);
+                    Console.WriteLine(" --- " + cr.Message.Value);
                     Console.WriteLine();
                 }
             }
-            catch (OperationCanceledException) {
+            catch (ConsumeException ex) {
+                Console.WriteLine($"Error Occured: {ex.Message}");
                 // Ctrl-C was pressed.
             }
             finally{
                 consumer.Close();
             }
         }
-
-        Console.Read();
     }
 }
